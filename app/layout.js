51e2fe12d6a8568685e8732f9424ad5f3b9b25e9
RayanEreteo <rayabf5@gmail.com>
2023-10-28 18:40:42 +0200

    correction du background

--- app/layout.js
+++ app/layout.js
@@ -4,13 +4,13 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Journal des rêves',
+  title: 'Dream Keeper',
   description: 'écriver vos rêves et améliorer votre rappel onirique',
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className='bg-blue-500'>
       <body className={inter.className}>{children}</body>
     </html>
   )
---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='app-wrapper w-[100vw] h-[100vh]'>
+      <h1 className='test'>Dream Keeper</h1>
+    </div>
+  )
+}
+
+export default page
