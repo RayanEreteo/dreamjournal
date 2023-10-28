58122a363d5cfa475a64bf554653600347bde73f
RayanEreteo <rayabf5@gmail.com>
2023-10-28 20:33:42 +0200

    modification de l'effet typewriter

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ function page() {
     <div className='app-wrapper w-[100vw] h-[100vh]'>
       <div className="title-wrapper text-white text-center m-auto w-fit">
         <h1 className='title text-9xl' id="title">
-          <Typewriter options={{autoStart: true, loop: false}} onInit={(typewriter) => {
+          <Typewriter options={{autoStart: true, loop: false, cursorClassName: "typewriter-cursor"}} onInit={(typewriter) => {
             typewriter.typeString("Dream Keeper").start()
           }}/>
         </h1>
