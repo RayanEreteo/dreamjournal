d2669e15f8a4e7764da28ef0752c033e023b389c
RayanEreteo <rayabf5@gmail.com>
2023-10-28 20:25:08 +0200

    ajout de l'effet 'typewriter' au titre

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+"use client"
+
+import Typewriter from "typewriter-effect"
 
 function page() {
   return (
     <div className='app-wrapper w-[100vw] h-[100vh]'>
-      <h1 className='test'>Dream Keeper</h1>
+      <div className="title-wrapper text-white text-center m-auto w-fit">
+        <h1 className='title text-9xl' id="title">
+          <Typewriter options={{autoStart: true, loop: false}} onInit={(typewriter) => {
+            typewriter.typeString("Dream Keeper").start()
+          }}/>
+        </h1>
+        <p className="text-3xl">Améliorer votre rappelle onirique.</p>
+      </div>
     </div>
   )
 }
-
 export default page
