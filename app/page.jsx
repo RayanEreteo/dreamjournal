4d0174c44addb32d726813810e222a3d95f9034f
RayanEreteo <rayabf5@gmail.com>
2023-10-30 22:45:59 +0100

    ajout de "framer motion"

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,19 @@
 "use client"
 
 import Button from "@/components/Button"
-import Typewriter from "typewriter-effect"
+import { motion } from "framer-motion"
 
 function page() {
   return (
     <div className='app-wrapper'>
       <div className="title-wrapper text-white text-center m-auto mt-[10rem] w-fit">
-        <h1 className='title text-9xl' id="title">
-          <Typewriter options={{autoStart: true, loop: false, cursorClassName: "typewriter-cursor"}} onInit={(typewriter) => {
-            typewriter.typeString("Dream Keeper").start()
-          }}/>
-        </h1>
+        <motion.div initial={{opacity: 0}} animate={{opacity: 1, y: -60}} transition={{delay: 0.20, type: 'spring'}} >
+          <h1 className='title text-9xl' id="title">Dream Keeper</h1>
+        </motion.div>
         <p className="text-3xl">Améliorer votre rappelle onirique.</p>
       </div>
       <div className="options-wrapper flex justify-center mt-5">
         <Button type={"primary"}>Se connecter</Button>
-        <Button type={"primary"}>Créer un compte</Button>
       </div>
     </div>
   )
