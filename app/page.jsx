3d151bdd3ec24b7befd5ca0b21ef6e02450ccb59
RayanEreteo <rayabf5@gmail.com>
2023-10-30 23:14:23 +0100

    ajout de l'animation du titre

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,27 @@
-"use client"
+"use client";
 
-import Button from "@/components/Button"
-import { motion } from "framer-motion"
+import Button from "@/components/Button";
+import { motion } from "framer-motion";
 
 function page() {
   return (
-    <div className='app-wrapper'>
+    <div className="app-wrapper">
       <div className="title-wrapper text-white text-center m-auto mt-[10rem] w-fit">
-        <motion.div initial={{opacity: 0}} animate={{opacity: 1, y: -60}} transition={{delay: 0.20, type: 'spring'}} >
-          <h1 className='title text-9xl' id="title">Dream Keeper</h1>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, y: -20 }}
+          transition={{ type: "spring", stiffness: 100 }}
+        >
+          <h1 className="title text-9xl" id="title">
+            Dream Keeper
+          </h1>
+          <p className="text-3xl">Améliorer votre rappelle onirique.</p>
         </motion.div>
-        <p className="text-3xl">Améliorer votre rappelle onirique.</p>
       </div>
       <div className="options-wrapper flex justify-center mt-5">
         <Button type={"primary"}>Se connecter</Button>
       </div>
     </div>
-  )
+  );
 }
-export default page
+export default page;
