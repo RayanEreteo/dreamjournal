b120fb9307388786c9fd9d08d762578a1f4ca622
RayanEreteo <rayabf5@gmail.com>
2023-11-04 18:36:08 +0100

    avancé design register

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React from "react";
 
 export const metadata = {
   title: "Créer un compte",
   description: "Créer un compte DreamKeeper",
-  author: "Rayan Ereteo"
-}
+  author: "Rayan Ereteo",
+};
 
 function Register() {
   return (
-    <div>page</div>
-  )
+    <div className="register-wrapper w-[100vw] h-[100vh] flex items-center justify-center">
+      <div className="form-container bg-white rounded h-[300px] flex items-center justify-center text-center">
+        <form action="">
+          <div className="input-fields flex flex-col">
+            <input type="email" placeholder="Votre email...." required />
+            <input
+              type="password"
+              placeholder="Votre mot de passe...."
+              required
+            />
+          </div>
+          <p>Combien de rêves vous souvenez vous par nuit ?</p>
+          <select>
+            <option value="">Aucun rêve</option>
+            <option value="">très rarement, 1 rêve par nuit</option>
+            <option value="">1 rêve toute les nuits</option>
+            <option value="">+1 rêve par nuit</option>
+          </select>
+          <button type="submit">S'inscrire</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
