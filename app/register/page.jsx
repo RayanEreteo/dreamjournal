7204c6b0d8dacf27ed03b3da2b86f00119a96ae3
RayanEreteo <rayabf5@gmail.com>
2023-11-04 18:52:51 +0100

    avancé du design "register"

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -9,24 +9,28 @@ export const metadata = {
 function Register() {
   return (
     <div className="register-wrapper w-[100vw] h-[100vh] flex items-center justify-center">
-      <div className="form-container bg-white rounded h-[300px] flex items-center justify-center text-center">
+      <div className="form-container bg-white rounded h-[300px] p-6 flex items-center justify-center text-center">
         <form action="">
           <div className="input-fields flex flex-col">
-            <input type="email" placeholder="Votre email...." required />
+            <input className="input-field" type="email" placeholder="Votre email...." required />
+            <hr />
             <input
+              className="input-field"
               type="password"
               placeholder="Votre mot de passe...."
               required
             />
+            <br />
           </div>
-          <p>Combien de rêves vous souvenez vous par nuit ?</p>
-          <select>
+          <p className="mb-6">Combien de rêves vous souvenez vous par nuit ?</p>
+          <select className="mb-6">
             <option value="">Aucun rêve</option>
             <option value="">très rarement, 1 rêve par nuit</option>
             <option value="">1 rêve toute les nuits</option>
             <option value="">+1 rêve par nuit</option>
           </select>
-          <button type="submit">S'inscrire</button>
+          <br />
+          <button type="submit" className="submit-button">S'inscrire</button>
         </form>
       </div>
     </div>
