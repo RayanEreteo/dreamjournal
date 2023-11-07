a03c0e9ff5b02d19a7f9f718ff08b1a3b30ae526
RayanEreteo <rayabf5@gmail.com>
2023-11-07 18:46:18 +0100

    chargement ajoutée

--- app/loading.jsx
+++ app/loading.jsx
@@ -10,7 +10,9 @@ function Loading() {
   };
 
   return (
-    <h1></h1>
+    <div className="loading-container flex items-center h-[100vh] justify-center">
+      <h1 className="text-9xl font-extrabold text-transparent bg-gradient-to-r bg-clip-text from-purple-400 to-pink-600">Chargement...</h1>
+    </div>
   );
 }
 
