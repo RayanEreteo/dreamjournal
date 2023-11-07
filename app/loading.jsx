c9da92ccb9d58562dcfa3dd1b243e46c5e11bb44
RayanEreteo <rayabf5@gmail.com>
2023-11-07 18:47:24 +0100

    changement mineur

--- app/loading.jsx
+++ app/loading.jsx
@@ -3,12 +3,6 @@
 import Image from "next/image";
 
 function Loading() {
-  const spinTransition = {
-    loop: Infinity,
-    ease: "linear",
-    duration: 1,
-  };
-
   return (
     <div className="loading-container flex items-center h-[100vh] justify-center">
       <h1 className="text-9xl font-extrabold text-transparent bg-gradient-to-r bg-clip-text from-purple-400 to-pink-600">Chargement...</h1>
