c724e9bc5cd7345a25f6839dcb3921d1835bbde3
RayanEreteo <rayabf5@gmail.com>
2023-11-07 18:35:32 +0100

    retrait du spinner

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+"use client"
+
+import Image from "next/image";
 
 function Loading() {
+  const spinTransition = {
+    loop: Infinity,
+    ease: "linear",
+    duration: 1,
+  };
+
   return (
-    <h1>Chargement</h1>
-  )
+    <h1></h1>
+  );
 }
 
-export default Loading
+export default Loading;
