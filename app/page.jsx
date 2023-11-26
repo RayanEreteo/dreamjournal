2001a72bc540f4413e10c4b1601a74139bfa00fb
RayanEreteo <rayabf5@gmail.com>
2023-11-26 17:27:56 +0100

    retrait des proscard

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Button from "@/components/Button";
-import Proscard from "@/components/Proscard";
 import { motion } from "framer-motion";
 
 function page() {
@@ -13,7 +12,7 @@ function page() {
         transition={{ type: "spring", stiffness: 100 }}
         className="Option-title-anim"
       >
-        <div className="title-wrapper text-white text-center m-auto mt-[10rem] w-fit">
+        <div className="title-wrapper text-white text-center m-auto mt-[20rem] w-fit">
           <h1
             className="sm:text-9xl title text-6xl font-extrabold text-transparent bg-gradient-to-r bg-clip-text from-purple-400 to-pink-600"
             id="title"
@@ -29,13 +28,6 @@ function page() {
           <Button type={"secondary"} urlPath={"/register"}>
             S'enregister
           </Button>
-        </div>
-        <div className="sm:block hidden benefits-container m-auto w-fit text-center mt-[10rem]">
-          <h1 className="sm:text-6xl text-white text-3xl">Pourquoi utiliser Dream Keeper ? </h1>
-          <div className="card-container flex flex-row items-center justify-around mt-[4rem]">
-            <Proscard prostitle={"Journal des rêves inclus."} prosdesc={"Créer un compte pour accéder à un journal des rêves et écrivez vos rêves au réveil sur n'importe quel appareil."}/>
-            <Proscard prostitle={"Découvrez des techniques de mémorisation."} prosdesc={"Découvrez plusieurs techniques pour se souvenir de vos rêves."}/>
-          </div>
         </div>
       </motion.div>
     </div>
