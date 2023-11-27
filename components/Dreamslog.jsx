ceac3e866feb3ea80f7742f436036cad8b58977b
RayanEreteo <rayabf5@gmail.com>
2023-11-27 10:59:46 +0100

    correction du responsive pour le dreamlog

--- components/Dreamslog.jsx
+++ components/Dreamslog.jsx
@@ -1,7 +1,7 @@
 function Dreamslog({ dream, day, islucid,deleteDream, id }) {
 
   return (
-    <div className={`dreamlog mb-3 border-solid border-2 ${islucid ? "border-purple-500" : "border-sky-500"} w-[430px] m-auto text-center`}>
+    <div className={`dreamlog sm:w-[430px] mb-3 border-solid border-2 ${islucid ? "border-purple-500" : "border-sky-500"} w-[330px] m-auto text-center`}>
         <h1>{dream}</h1>
         <hr />
         <p>{day}</p>
